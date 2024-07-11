7f5576e4dee8993213c57c7bcd8571ba6ee1469f
Thomas <dgbaoquoc@gmail.com>
2024-07-11 20:33:38 +0700

    fix: adapter vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,7 +10,5 @@ export default defineConfig({
     applyBaseStyles: false
   }), react()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel()
 });
